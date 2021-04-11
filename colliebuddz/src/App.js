89813d0ccf76b447c04e7492723c243fd41e9ef9
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-04-10 18:49:17 -0700

    beginning parent child component structure with props as data model in parent

--- colliebuddz/src/App.js
+++ colliebuddz/src/App.js
@@ -1,8 +1,36 @@
 import './App.css'
+import Summary from './Summary.js'
+import BusinessStats from './BusinessStats.js'
+import ConsumerStats from './ConsumerStats.js'
+
+const data = {
+    summary: {
+      name: 'Sour Diesel',
+      image: '',
+      description: ''
+    },
+    businessStats: {
+      difficulty: '',
+      yield: '',
+      flowering: ''
+    },
+    consumerStats: {
+      effects: {
+        first: 34,
+        second: 63,
+        third: 78
+      }
+    }
+}
 
 function App() {
   return (
-    <div>Hi</div>
+    <>
+      <div>Hi</div>
+      < Summary data={data.summary} />
+      {/* < BusinessStats data={data.businessStats} />
+      < ConsumerStats data={data.consumerStats} /> */}
+    </>
   )
 }
 
---
+++ colliebuddz/src/Summary.js
@@ -0,0 +1,9 @@
+const Summary = (props) => {
+
+    console.log(props)
+    return (
+        <div>Hi again {props.data.name}</div>
+    )
+}
+
+export default Summary
