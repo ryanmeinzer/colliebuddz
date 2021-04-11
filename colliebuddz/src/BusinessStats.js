f868616ccaf25dba33e022773e1e10b456188aaa
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-04-11 12:31:39 -0700

    added titles to all fields and capitalized respectively

--- colliebuddz/src/BusinessStats.js
+++ colliebuddz/src/BusinessStats.js
@@ -2,11 +2,11 @@ const BusinessStats = (props) => {
 
     console.log(props)
     return (
-        <>
-            <div>{props.data.difficulty}</div>
-            <div>{props.data.yield}</div>
-            <div>{props.data.flowering}</div>
-        </>
+        <div>
+            <div>Growing Difficulty: {props.data.difficulty}</div>
+            <div>Yield: {props.data.yield}</div>
+            <div>Flowering: {props.data.flowering}</div>
+        </div>
     )
 }
 
--- colliebuddz/src/ConsumerStats.js
+++ colliebuddz/src/ConsumerStats.js
@@ -1,12 +1,11 @@
 const ConsumerStats = (props) => {
 
-    console.log(props)
     return (
-        <>
-            <div>{Object.values(props.data)[0]}</div>
-            <div>{Object.values(props.data)[1]}</div>
-            <div>{Object.values(props.data)[2]}</div>
-        </>
+        <div>
+            <div>{Object.keys(props.data)[0][0].toUpperCase() + Object.keys(props.data)[0].substring(1)}: {Object.values(props.data)[0]}</div>
+            <div>{Object.keys(props.data)[1][0].toUpperCase() + Object.keys(props.data)[1].substring(1)}: {Object.values(props.data)[1]}</div>
+            <div>{Object.keys(props.data)[2][0].toUpperCase() + Object.keys(props.data)[2].substring(1)}: {Object.values(props.data)[2]}</div>
+        </div>
     )
 }
 
